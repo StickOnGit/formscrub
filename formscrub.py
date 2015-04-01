0b4bc1b25404d298bb7db31ba4af1caf7079f3d0
StickOnGit <the87thbombfish@gmail.com>
2015-04-01 08:29:36 -0500

    less recursive (smarter)

--- formscrub.py
+++ formscrub.py
@@ -1,69 +1,67 @@
 #! /usr/bin/env python
 
-"""formscrub deletes the mp_uuid key-value pairs from imported .itpl files.
-The resulting .itpl file will always be treated as a new form with new form sections.
-This file can be imported over and over; it will always be treated as a new form."""
-print
-##u mad bro?
-def end():
-	print
-	sys.exit(0)
-##end u mad bro
+"""Deletes every uuid found in .itpl files.
 
-try:
-	import sys
-	import plistlib as PLIB
-except ImportError as reason:
-	print """\tCouldn't run formscrub because: {reason}.
-\tIf you're missing a module, you may need to download 'pip' or a similar Python package installer and then run 'pip install (missing module)'.
-	
-\tGoogle is your friend - use it! Use it now!!""".format(reason=reason)
-	end()
+Once the file is 'scrubbed' it can be imported and 
+the software automatically assigns new uuids where necessary.
 
-def scrub(someobject, badvalue='mp_uuid'):
-	"""Walks the structure of the plistlib-created dict and finds all the mpuuids and viciously eliminates them.
-	
-	Can optionally be passed a different key to search for."""
-	if isinstance(someobject, dict):
-		for k in someobject.keys():
-			if k == badvalue:
-				del someobject[k]
-				_COUNT[0] += 1
-			else:
-				scrub(someobject[k], badvalue)
-	elif isinstance(someobject, list):
-		for i in reversed(range(len(someobject))):
-			if someobject[i] == badvalue:
-				del someobject[i]
-				_COUNT[0] += 1
-			else:
-				scrub(someobject[i], badvalue)
-	else:
-		pass
-	
-	return "\tRemoved {count} instances of {badvalue} from {file}.".format(count=_COUNT[0], badvalue=badvalue, file=file)
+The file it produces should NOT be used outside the office.
+"""
+
+import sys
+import plistlib as PLIB
+
+def plog(msg):
+    print "\t{}".format(msg)
+
+def end(*out_msg):
+    if out_msg:
+        print
+        for msg in out_msg:
+            plog(msg)
+    print
+    sys.exit(0)
+
+def scrub(template, bad='mp_uuid'):
+    """Looks through the itpl for uuids and deletes them.
+    
+    Returns the number of deleted ids.
+    """
+    def clear(seq):
+        """Deletes bad value in sequence if present.
+        
+        Returns 1 if deleted, 0 if not found.
+        """
+        if bad in seq:
+            del seq[bad]
+            return 1
+        return 0
+    
+    count = clear(template)
+    for section in template.get('iformSectionTiesArray', []):
+        if 'iform_section' in section:
+            sub_sec = section['iform_section']
+            count += clear(sub_sec)
+            for element in sub_sec.get('iformFieldsArray', []):
+                count += clear(element)
+    return count
 
 if __name__ == '__main__':
-	_COUNT = [0]
-	try:
-		file = sys.argv[1]
-	except IndexError:
-		print "\tformscrub needs the filename. Try 'formscrub myfile.itpl'."
-		end()
+    if not sys.argv[1:]:
+        end("Please provide the file name.")
+    temp = sys.argv[1]
 
-	try:
-		loadedplist = PLIB.readPlist(file)
-	except:
-		print "\tWasn't able to read '{file}'. Check the file and try again.".format(file=file)
-		end()
-		
-	print "\t{file} is loaded. Scrubbing...".format(file=file)
-	result = scrub(loadedplist)
-	print result
-	try:
-		PLIB.writePlist(loadedplist, file)
-		print "\tSuccessfully saved '{file}'.".format(file=file)
-	except:
-		print "\tCouldn't seem to save {file} after scrubbing it.".format(file=file)
-		end()
-	end()
+    try:
+        itpl = PLIB.readPlist(temp)
+    except:
+        end("Couldn't load the file. Exiting.")
+        
+    result = scrub(itpl)
+    first_msg = "{} ids scrubbed.".format(result)
+    second_msg = "Successfully saved {}.".format(temp)
+    try:
+        PLIB.writePlist(itpl, temp)
+    except:
+        second_msg = "Couldn't save after scrubbing."
+    
+    end(first_msg, second_msg)
